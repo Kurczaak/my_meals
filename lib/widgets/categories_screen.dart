5692f5f179945d1d9334722dbb6249385d3508f7
kurczak <kurczakmichal97@gmail.com>
2020-08-20 19:09:58 +0100

    Just grid with categories. No functionality.

---
+++ lib/widgets/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Meals App'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        children: [
+          ...DUMMY_CATEGORIES.map((category) {
+            return CategoryItem(
+              category.title,
+              category.color,
+            );
+          }).toList()
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
